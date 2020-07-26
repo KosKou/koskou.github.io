e0f04fd83631d4dde5be24a7d26bb89fe69e7a10
Taku <koskoudev@gmail.com>
2020-07-25 19:56:49 -0500

    Updates

---
+++ precache-manifest.e61a6d42cd14d6c1ba4f2e9c27dc150a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53a759e5ce8cefe9cdeaf66a6c60b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec111fb1425551b84a64",
+    "url": "/static/css/main.9da775e4.chunk.css"
+  },
+  {
+    "revision": "4c8821df51cc8acf6513",
+    "url": "/static/js/2.629003ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.629003ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec111fb1425551b84a64",
+    "url": "/static/js/main.6cc50e93.chunk.js"
+  },
+  {
+    "revision": "7c4528e79d11514eb981",
+    "url": "/static/js/runtime-main.d6ff568d.js"
+  },
+  {
+    "revision": "06f749f145eaaad2fff2cc09c1b6f9a9",
+    "url": "/static/media/Agile.06f749f1.png"
+  },
+  {
+    "revision": "e696625f659d8e110a435f6af4778d17",
+    "url": "/static/media/Automation.e696625f.png"
+  },
+  {
+    "revision": "6b89092b302c3a5aca4486df7d19c662",
+    "url": "/static/media/BanneGonzalesWhite.6b89092b.png"
+  },
+  {
+    "revision": "d8249d4fc8a9cf1dcf58f3ee95115e0d",
+    "url": "/static/media/BannerPic.d8249d4f.jpg"
+  },
+  {
+    "revision": "514014735dcdb0d67eeddfe1cd1e27f0",
+    "url": "/static/media/DatabaseSQL.51401473.png"
+  },
+  {
+    "revision": "f60ac029d04ed8613ccd8bb686f4a86a",
+    "url": "/static/media/EN-CV-BANNERGONZALES.f60ac029.pdf"
+  },
+  {
+    "revision": "b36cb60f6e73e53d521d4bb74bc87940",
+    "url": "/static/media/Git.b36cb60f.png"
+  },
+  {
+    "revision": "d947047e7224376caab2b456d8ceff63",
+    "url": "/static/media/Landscape.d947047e.jpg"
+  },
+  {
+    "revision": "682266074f1f5c28eace0fd98a2e5c19",
+    "url": "/static/media/MongoDB.68226607.png"
+  },
+  {
+    "revision": "4f294013b04ce81b3195feabc6160262",
+    "url": "/static/media/ReactJS.4f294013.png"
+  },
+  {
+    "revision": "0ad9d8ba6a2e5cb824ccbe10951db428",
+    "url": "/static/media/Reactor.0ad9d8ba.png"
+  },
+  {
+    "revision": "2dacd8d5b9b35a4ccd3ca10a05f29a77",
+    "url": "/static/media/SAP.2dacd8d5.png"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/static/media/arrow_right.d9ccc77c.svg"
+  }
+]);
