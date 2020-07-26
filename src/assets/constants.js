90ba4685e7f1453bc03383096fd91361624cf52b
Taku <koskoudev@gmail.com>
2020-07-25 20:16:49 -0500

    Changes

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -4,6 +4,8 @@ export const TWITTER_LINK = 'https://twitter.com/bannergz'
 export const YOUTUBE_LINK = 'https://www.youtube.com'
 export const GITHUB_LINK = 'https://github.com/koskou'
 
-export const ABOUT_TEXT = "Hey there! I'm a FullStack Developer from Perú who enjoys working with " +
+export const ABOUT_TEXT = "Hey there! I'm a Peruvian FullStack Developer who enjoys" +
+    " working" +
+    " with " +
     "most of top technology trends as microservices, cloud computing and reactive " +
     "programming"
